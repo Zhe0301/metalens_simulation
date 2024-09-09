6b4739f832e8984828b5255c9f259af9cb9865b2
WangzheZhou <zwz0301@hotmail.com>
2024-09-09 22:21:08 +0800

    更新了Len类，binary2_d函数支持输入任意单元结构相位和透过率

--- Lens.py
+++ Lens.py
@@ -20,26 +20,25 @@ matplotlib.use('qt5agg')
 
 
 class Lens:
-    def __init__(self, Grid, t=1):
+    def __init__(self, Grid):
         """
-        Grid网格类，
-        t光强透过率
+        :param: Grid网格类，
+        :param: t光强透过率
         """
-        self.phase_2pi = None
         self.Grid = Grid
-        self.t = t
         self.complex_amplitude_t = None
         self.phase = None
         self.mask = None
         self.amplitude = None
 
-    def binary2(self, r_max, m, r_0, a):
+    def binary2(self, r_max, m, r_0, a, t=1):
         """
         二元面2模拟:使用镜像坐标的偶次多项式描述相位面
-        r_max最大半径,单位 mm;
-        m衍射阶数;
-        r_0归一化半径,单位 mm;
-        a多项式系数数组
+        :param: r_max最大半径,单位 mm;
+        :param: m衍射阶数;
+        :param: r_0归一化半径,单位 mm;
+        :param: a多项式系数数组;
+        :param: t 能量透过率
         """
         mask_index = self.Grid.d2_r <= r_max
         self.mask = np.zeros_like(self.Grid.d2_r)
@@ -49,72 +48,95 @@ class Lens:
         for i, a_i in enumerate(a):  # enumerate组合为一个索引序列，同时列出数据和数据下标
             self.phase += m * a_i * (self.Grid.d2_r / r_0) ** (2 * (i + 1))
         self.phase = self.phase * self.mask  # zemax中相位图以角度为单位，此处以弧度
-        self.phase_2pi = np.mod(self.phase, 2 * np.pi) * self.mask  # 相位，取余数到0到2pi
         self.amplitude = np.ones_like(self.Grid.d2_r) * self.mask
-        self.complex_amplitude_t = self.amplitude * np.exp(1j * self.phase) * self.t * self.mask  # 复振幅透过率
+        self.complex_amplitude_t = self.amplitude * np.exp(1j * self.phase) * t * self.mask  # 复振幅透过率
 
-    def binary2_d(self, r_max, m, r_0, a, d=8):
+    def binary2_d(self, r_max, m, r_0, a, unit_phase=None, unit_t=None,  boundaries=None):
         """
         相位离散型的二元面2，
-        r_max最大半径,
-        m衍射阶数,归一化半径r_0,单位 mm,多项式系数数组a，d相位的均分份数
+        :param r_max:  镜片最大半径;
+        :param m: 衍射阶数,d相位的均分份数;
+        :param r_0: 归一化半径，单位mm
+        :param a: 多项式系数数组;
+        :param unit_phase: 数组，离散单元的相位值
+        :param unit_t: 数组，离散单元的能量透过率
         """
         # 相位离散型二元面2，最大半径r_max,衍射阶数m,归一化半径r_0,单位 mm,多项式系数数组a，d相位的均分份数
+        if unit_phase is None:
+            unit_phase = np.array([0, 1 / 4, 2 / 4, 3 / 4, 1, 5 / 4, 6 / 4, 7 / 4]) * np.pi + np.pi / 8
+        if unit_t is None:
+            unit_t = np.array([1, 1, 1, 1, 1, 1, 1, 1])
+        if  boundaries is None:
+            partition_range = np.array([[0, 1 / 8], [1 / 8, 2 / 8], [2 / 8, 3 / 8], [3 / 8, 4 / 8], [4 / 8, 5 / 8],
+                                        [5 / 8, 6 / 8], [6 / 8, 7 / 8], [7 / 8, 1]]) * 2 * np.pi
         mask_index = self.Grid.d2_r <= r_max
         self.mask = np.zeros_like(self.Grid.d2_r)
         self.mask[mask_index] = 1
         self.phase = np.zeros_like(self.Grid.d2_r)
-
+        self.amplitude = np.ones_like(self.Grid.d2_r) * self.mask
         for i, a_i in enumerate(a):  # enumerate组合为一个索引序列，同时列出数据和数据下标
             self.phase += m * a_i * (self.Grid.d2_r / r_0) ** (2 * (i + 1))
-          # zemax中相位图以角度为单位，此处以弧度
-        self.phase_2pi = np.mod(self.phase, 2 * np.pi)
-        if d > 0:
-            interval = 2 * np.pi / d
+        # zemax中相位图以角度为单位，此处以弧度
+        self.phase = np.mod(self.phase, 2 * np.pi)
+        if len(unit_phase) == 0 or len(unit_t) == 0 or len(boundaries) == 0:
             # 计算离散相位
-            # self.phase_num = np.floor( self.phase_2pi / interval)
-            self.phase_2pi = (np.floor( self.phase_2pi / interval) + 1 / 2) * interval
-            self.phase_2pi =  self.phase_2pi * self.mask
-            self.phase = self.phase_2pi
-        else:
             print("需输入正确的离散值,结果未离散")
             self.phase = self.phase * self.mask
-        self.amplitude = np.ones_like(self.Grid.d2_r) * self.mask
-        self.complex_amplitude_t = self.amplitude * np.exp(1j *  self.phase) * self.t * self.mask  # 复振幅透过率
+        else:
+            for i, b in enumerate(boundaries):
+                mask = (self.phase >= b[0]) & (self.phase < b[1])
+                self.phase[mask] = unit_phase[i]
+                self.amplitude[mask] = np.sqrt(unit_t[i])
+            self.phase = self.phase * self.mask
+
+        self.complex_amplitude_t = self.amplitude * np.exp(1j * self.phase) * self.mask  # 复振幅透过率
 
     def ideal_lens(self, r_max, focal_length, wavelength_vacuum):
+        """
+        理想透镜
+        :param r_max: 镜片最大半径;
+        :param focal_length: 焦距;
+        :param wavelength_vacuum: 真空中波长
+        """
         mask_index = self.Grid.d2_r <= r_max
         self.mask = np.zeros_like(self.Grid.d2_r)
         self.mask[mask_index] = 1
         self.phase = np.zeros_like(self.Grid.d2_r)
         self.phase = -(focal_length - np.sqrt(focal_length ** 2 + self.Grid.d2_r ** 2)) * 2 * np.pi / wavelength_vacuum
         self.phase = self.phase * self.mask
-        self.phase_2pi = (self.phase % (2 * np.pi)) * self.mask  # 相位，取余数到0到2pi
         self.amplitude = np.ones_like(self.Grid.d2_r) * self.mask
         self.amplitude = np.ones_like(self.Grid.d2_r)
         self.complex_amplitude_t = self.amplitude * np.exp(- 1j * self.phase) * self.t * self.mask
         # self.complex_amplitude_t = self.amplitude * np.exp(-1j * self.phase) * self.t
 
     def hole(self, r_max):
+        """
+        圆孔
+        :param r_max: 孔的半径;
+        """
         mask_index = self.Grid.d2_r <= r_max
         self.mask = np.zeros_like(self.Grid.d2_r)
         self.mask[mask_index] = 1
         self.amplitude = np.ones_like(self.Grid.d2_r) * self.mask
         self.phase = np.zeros_like(self.Grid.d2_r) * self.mask
-        self.phase_2pi = (self.phase % (2 * np.pi)) * self.mask  # 相位，取余数到0到2pi
         self.complex_amplitude_t = self.amplitude * np.exp(1j * self.phase) * self.t * self.mask
 
     def square_hole(self, lenth_x, lenth_y):
+        """
+        方孔
+        :param lenth_x: 孔的x长度;
+        :param lenth_y: 孔的y长度;
+        """
         mask_index = (abs(self.Grid.d2_x) <= lenth_x / 2) & (abs(self.Grid.d2_x) >= - lenth_x / 2) & (
-                    self.Grid.d2_y <= lenth_y / 2) & (self.Grid.d2_y >= - lenth_y / 2)
+                self.Grid.d2_y <= lenth_y / 2) & (self.Grid.d2_y >= - lenth_y / 2)
         self.mask = np.zeros_like(self.Grid.d2_r)
         self.mask[mask_index] = 1
         self.amplitude = np.ones_like(self.Grid.d2_r) * self.mask
         self.phase = np.zeros_like(self.Grid.d2_r) * self.mask
-        self.phase_2pi = (self.phase % (2 * np.pi)) * self.mask  # ��位，取余数到0到2pi
         self.complex_amplitude_t = self.amplitude * np.exp(1j * self.phase) * self.t * self.mask
 
     def plot_phase(self, save_path=r'/'):
+        phase_2pi = np.mod(self.phase, 2 * np.pi)
         plt.figure(figsize=(16, 9))
         plt.subplot(1, 2, 1)
         plt.pcolormesh(self.Grid.d2_x, self.Grid.d2_y, self.phase, cmap="jet")
@@ -124,7 +146,7 @@ class Lens:
         cb = plt.colorbar()
         cb.set_label(r'Phase(rad)')  # 给colorbar添加标题
         plt.subplot(1, 2, 2)
-        plt.pcolormesh(self.Grid.d2_x, self.Grid.d2_y, self.phase_2pi, cmap="jet")
+        plt.pcolormesh(self.Grid.d2_x, self.Grid.d2_y, phase_2pi, cmap="jet")
         plt.title(r'Phase Distribution $0 ~ 2\pi$')
         plt.xlabel(r'$x$(mm)')
         plt.ylabel(r'$y$(mm)')
--- RunSimulation.py
+++ RunSimulation.py
@@ -32,26 +32,34 @@ from Source import Source
 from PropOperator import PropOperator
 from Tools import *
 
-
-
 # 建立仿真网络
 
 period = 347e-6  # 按单元结构周期仿真mm
 length = 2
-num_points = round(length/period)
+num_points = round(length / period)
 # num_points = 4096
 
-G = Grid(num_points, num_points*period)
+G = Grid(num_points, num_points * period)
 efl = [0.7, 1.4, 2.1, 2.8, 3.5, 4.2]  # 有效焦距，单位mm，用于图片命名
-d_12 = [1.100000006257,1.785825240396,2.083347319444,2.25568813143,2.364753377126,2.386287993685]
-d_23 = [2.924563548595,2.020781241757,1.568999114497,1.26635292265,1.031953399776,7.999999810325e-1]
-d_bfl = [1.975436417502,2.193393520095,2.347653672864,2.477959067576,2.60329289574,2.813712156567]
+d_12 = [1.100000006257, 1.785825240396, 2.083347319444, 2.25568813143, 2.364753377126, 2.386287993685]
+d_23 = [2.924563548595, 2.020781241757, 1.568999114497, 1.26635292265, 1.031953399776, 7.999999810325e-1]
+d_bfl = [1.975436417502, 2.193393520095, 2.347653672864, 2.477959067576, 2.60329289574, 2.813712156567]
 # 仿真参数
 wavelength_vacuum = 632.8 * 1e-6  # 真空波长，单位mm
 refractive_index = 1.457  # SiO2折射率
-
+name = "Actual cylinder"
 # 相位离散量
 discrete = 8
+unit_phase = np.linspace(0, 2 * np.pi, discrete, endpoint=False) + np.pi / discrete
+unit_t = np.ones_like(unit_phase)
+points = np.linspace(0, 1, discrete + 1)
+boundaries = np.array([[points[i], points[i + 1]] for i in range(discrete)])
+boundaries = boundaries * 2 * np.pi
+# 实际离散
+# unit_phase = []
+# unit_t = []
+# points = np.linspace(0, 1, discrete + 1)
+# boundaries = np.array([[points[i], points[i + 1]] for i in range(discrete)])
 # 建立仿真目录
 current_date = datetime.now().strftime('%Y%m%d')
 # %y 两位数的年份表示（00-99）
@@ -63,8 +71,8 @@ current_date = datetime.now().strftime('%Y%m%d')
 # %M 分钟数（00=59）
 # %S 秒（00-59）
 
-save_path = r'E:/Research/WavePropagation/metalens_simulation/Zoom_6×/{}_discrete_{}/'.format(current_date,
-                                                                                              discrete)  # 数据存放目录
+save_path = r'E:/Research/WavePropagation/metalens_simulation/Zoom_6×/{}_{}/'.format(current_date,
+                                                                                              name)  # 数据存放目录
 if not os.path.exists(save_path):
     # 如果不存在则创建
     os.makedirs(save_path)
@@ -73,9 +81,12 @@ else:
     print(f"文件夹 '{save_path}' 已存在。")
 # 建立log
 for i in range(len(efl)):
-    logger.add(save_path+'f_{:.1f}.log'.format(efl[i]))
+    logger.add(save_path + 'f_{:.1f}.log'.format(efl[i]))
     logger.info("EFFL = {:.1f} mm".format(efl[i]))
-    logger.info("simulation mesh:{}×{}. simulation lenth:{:.5f} mm".format(num_points, num_points,num_points*period))
+    logger.info("simulation mesh:{}×{}  simulation lenth:{:.5f} mm".format(num_points, num_points, num_points * period))
+    logger.info("unit phase = {}".format(unit_phase))
+    logger.info("unit transmittance = {}".format(unit_t))
+    logger.info("boundaries = {}".format(boundaries))
 
     # 光源
     s = Source(G.d2_x, G.d2_y, wavelength_vacuum, 1)
@@ -84,22 +95,21 @@ for i in range(len(efl)):
     # 镜片
     L1 = Lens(G)
     L1.binary2_d(0.375, 1, 1, [-1.134335348014e3, 1.997482968732e2, -7.744689756243e2,
-                              2.350435931257e3, 3.245672141492e3, -2.507104417945e4], d=discrete)
+                               2.350435931257e3, 3.245672141492e3, -2.507104417945e4], unit_phase, unit_t, boundaries)
 
-    np.save(save_path + "Lens1.npz",L1.phase_2pi)
+    np.save(save_path + "Lens1.npz", L1.phase)
     # L1.plot_phase(save_path + 'L1_d_')
     L2 = Lens(G)
     L2.binary2_d(0.25, 1, 1, [6.658448719319e3, -1.180564189085e4, 3.123599533268e5,
-                              -8.915821224249e6, 1.353360477873e8, -8.261794056944e8], d=discrete)
-    np.save(save_path + "Lens2.npz", L2.phase_2pi)
+                              -8.915821224249e6, 1.353360477873e8, -8.261794056944e8], unit_phase, unit_t, boundaries)
+    np.save(save_path + "Lens2.npz", L2.phase)
     # L2.plot_phase(save_path + 'L2_d_')
     L3 = Lens(G)
     L3.binary2_d(0.95, 1, 1, [-3.655457426687e3, 2.169008952537e2, -1.402136806599e2,
-                              2.517459986023e2, -2.498340816648e2, 9.683857736021e1], d=discrete)
-    np.save(save_path + "Lens3.npz", L3.phase_2pi)
+                              2.517459986023e2, -2.498340816648e2, 9.683857736021e1], unit_phase, unit_t, boundaries)
+    np.save(save_path + "Lens3.npz", L3.phase)
     # L3.plot_phase(save_path + 'L3_d_')
     d_lens = 0.6
-
 
     # method = 'FFT-DI'
     # method = 'AS'
